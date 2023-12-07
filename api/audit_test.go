99adac14707954929be133d97e7a1f29c96a341d
sheyanjie <yjshe@taosdata.com>
2023-12-07 13:55:08 +0800

    update test

--- api/audit_test.go
+++ api/audit_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/taosdata/taoskeeper/infrastructure/config"
 )
 
-func TestAudit(t *testing.T) {
+func getCfg() *config.Config {
 	c := &config.Config{
 		Port: 6043,
 		TDengine: config.TDengineRestful{
@@ -28,6 +28,11 @@ func TestAudit(t *testing.T) {
 			},
 		},
 	}
+	return c
+}
+func TestAudit(t *testing.T) {
+	c := getCfg()
+
 	a, err := NewAudit(c)
 	assert.NoError(t, err)
 	err = a.Init(router)
@@ -82,36 +87,10 @@ func TestAudit(t *testing.T) {
 			assert.Equal(t, c.expect, data.Data[0][1])
 		})
 	}
-}
 
-func TestAuditBatch(t *testing.T) {
-	c := &config.Config{
-		Port: 6043,
-		TDengine: config.TDengineRestful{
-			Host:     "127.0.0.1",
-			Port:     6041,
-			Username: "root",
-			Password: "taosdata",
-		},
-		Audit: config.AuditConfig{
-			Database: config.Database{
-				Name: "audit",
-			},
-		},
-	}
-	a, err := NewAudit(c)
-	assert.NoError(t, err)
-	err = a.Init(router)
-	assert.NoError(t, err)
-
+	// test audit batch
 	input := `{"records": [{"timestamp": "1699839716440000000", "cluster_id": "cluster_id_batch", "user": "user", "operation": "operation", "db":"dbnamea", "resource":"resourcenamea", "client_add": "localhost:30000", "details": "detail"},` +
 		`{"timestamp": "1699839716441000000", "cluster_id": "cluster_id_batch", "user": "user", "operation": "operation", "db":"dbnamea", "resource":"resourcenamea", "client_add": "localhost:30000", "details": "detail"}]}`
-
-	conn, err := db.NewConnectorWithDb(c.TDengine.Username, c.TDengine.Password, c.TDengine.Host, c.TDengine.Port, c.Audit.Database.Name)
-	assert.NoError(t, err)
-	defer func() {
-		_, _ = conn.Query(context.Background(), "drop stable if exists audit.operations")
-	}()
 
 	t.Run("testbatch", func(t *testing.T) {
 
@@ -134,5 +113,4 @@ func TestAuditBatch(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, 2, len(data.Data))
 	})
-
 }
