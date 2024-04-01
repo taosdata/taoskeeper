8c91e6d77d069830b63a6591abca19d202f5f760
menshibin <men_shi_bin@163.com>
2024-04-01 14:31:03 +0800

    Resolve database construction conflicts

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -1,9 +1,12 @@
 package monitor
 
 import (
+	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/taosdata/go-utils/web"
 	"github.com/taosdata/taoskeeper/api"
+	"github.com/taosdata/taoskeeper/db"
 	"github.com/taosdata/taoskeeper/infrastructure/config"
 	"github.com/taosdata/taoskeeper/infrastructure/log"
 	"testing"
@@ -29,6 +32,11 @@ func TestStart(t *testing.T) {
 	for k, _ := range SysMonitor.outputs {
 		SysMonitor.Deregister(k)
 	}
+
+	conn, err := db.NewConnectorWithDb(conf.TDengine.Username, conf.TDengine.Password, conf.TDengine.Host, conf.TDengine.Port, conf.Metrics.Database)
+	assert.NoError(t, err)
+	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", conf.Metrics.Database))
+
 }
 
 func TestParseUint(t *testing.T) {
