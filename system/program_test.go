d84273fd2a20d7c4a4abb60293bf644823c63e7f
menshibin <men_shi_bin@163.com>
2024-04-01 15:43:28 +0800

    Resolve database construction conflicts

--- system/program_test.go
+++ system/program_test.go
@@ -1,11 +1,21 @@
 package system
 
 import (
+	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/taosdata/taoskeeper/db"
+	"github.com/taosdata/taoskeeper/infrastructure/config"
 	"testing"
 )
 
 func TestStart(t *testing.T) {
+	conf := config.InitConfig()
 	server := Init()
 	assert.NotNil(t, server)
+
+	conn, err := db.NewConnectorWithDb(conf.TDengine.Username, conf.TDengine.Password, conf.TDengine.Host, conf.TDengine.Port, conf.Metrics.Database)
+	assert.NoError(t, err)
+	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", conf.Metrics.Database))
+	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", conf.Audit.Database))
 }
