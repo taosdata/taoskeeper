4dd660658d51d462708435ef1f9b52bcb3a905df
menshibin <men_shi_bin@163.com>
2024-04-01 15:59:34 +0800

    Resolve database construction conflicts

--- system/program_test.go
+++ system/program_test.go
@@ -10,12 +10,11 @@ import (
 )
 
 func TestStart(t *testing.T) {
-	conf := config.InitConfig()
 	server := Init()
 	assert.NotNil(t, server)
 
-	conn, err := db.NewConnectorWithDb(conf.TDengine.Username, conf.TDengine.Password, conf.TDengine.Host, conf.TDengine.Port, conf.Metrics.Database)
+	conn, err := db.NewConnectorWithDb(config.Conf.TDengine.Username, config.Conf.TDengine.Password, config.Conf.TDengine.Host, config.Conf.TDengine.Port, config.Conf.Metrics.Database)
 	assert.NoError(t, err)
-	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", conf.Metrics.Database))
-	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", conf.Audit.Database))
+	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", config.Conf.Metrics.Database))
+	conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", config.Conf.Audit.Database))
 }
