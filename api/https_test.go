2d6ffd2b3c2b30c7e55fa6ffd5c277f91a797392
sheyanjie-qq <249478495@qq.com>
2024-05-24 09:25:57 +0800

    mod unit test

--- api/https_test.go
+++ api/https_test.go
@@ -13,10 +13,8 @@ import (
 	"log"
 	"math/big"
 	"net/http"
-	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 	"testing"
 	"time"
 
@@ -35,43 +33,15 @@ func TestHttps(t *testing.T) {
 
 	CreateDatabase(cfg.TDengine.Username, cfg.TDengine.Password, cfg.TDengine.Host, cfg.TDengine.Port, cfg.TDengine.Usessl, cfg.Metrics.Database, cfg.Metrics.DatabaseOptions)
 
-	gm := NewGeneralMetric(cfg)
-	err := gm.Init(router)
-	assert.NoError(t, err)
-
-	testcfg := struct {
-		name   string
-		ts     int64
-		tbname string
-		data   string
-		expect string
-	}{
-		name:   "1",
-		tbname: "taosd_cluster_basic",
-		ts:     1705655770381,
-		data:   `{"ts":"1705655770381","cluster_id":"7648966395564416484","protocol":2,"first_ep":"ssfood06:6130","first_ep_dnode_id":1,"cluster_version":"3.2.1.0.alp"}`,
-		expect: "7648966395564416484",
-	}
-
-	conn, err := db.NewConnectorWithDb(gm.username, gm.password, gm.host, gm.port, gm.database, gm.usessl)
+	conn, err := db.NewConnectorWithDb(cfg.TDengine.Username, cfg.TDengine.Password, cfg.TDengine.Host, cfg.TDengine.Port, cfg.Metrics.Database, cfg.TDengine.Usessl)
 	assert.NoError(t, err)
 	defer func() {
-		_, _ = conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", gm.database))
+		_, _ = conn.Query(context.Background(), fmt.Sprintf("drop database if exists %s", cfg.Metrics.Database))
 	}()
 
-	t.Run(testcfg.name, func(t *testing.T) {
-		w := httptest.NewRecorder()
-		body := strings.NewReader(testcfg.data)
-		req, _ := http.NewRequest(http.MethodPost, "/taosd-cluster-basic", body)
-		router.ServeHTTP(w, req)
-		assert.Equal(t, 200, w.Code)
-
-		data, err := conn.Query(context.Background(), fmt.Sprintf("select ts, cluster_id from %s.%s where ts=%d", gm.database, testcfg.tbname, testcfg.ts))
-		assert.NoError(t, err)
-		assert.Equal(t, 1, len(data.Data))
-		assert.Equal(t, testcfg.expect, data.Data[0][1])
-	})
-
+	data, err := conn.Query(context.Background(), "select server_version()")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(data.Data))
 }
 
 func generateSelfSignedCert() (tls.Certificate, error) {
