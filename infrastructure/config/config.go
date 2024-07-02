026789a3feeaf65dac1a532a83c14be05baee8de
sheyanjie-qq <249478495@qq.com>
2024-07-02 19:20:36 +0800

    add database option default cfg

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -188,6 +188,22 @@ func init() {
 	_ = viper.BindEnv("metrics.database.name", "TAOS_KEEPER_METRICS_DATABASE")
 	pflag.String("metrics.database.name", "log", `database for storing metrics data. Env "TAOS_KEEPER_METRICS_DATABASE"`)
 
+	viper.SetDefault("metrics.database.options.vgroups", 1)
+	_ = viper.BindEnv("metrics.database.options.vgroups", "TAOS_KEEPER_METRICS_VGROUPS")
+	pflag.Int("metrics.database.options.vgroups", 1, `database option vgroups for audit database. Env "TAOS_KEEPER_METRICS_VGROUPS"`)
+
+	viper.SetDefault("metrics.database.options.buffer", 64)
+	_ = viper.BindEnv("metrics.database.options.buffer", "TAOS_KEEPER_METRICS_BUFFER")
+	pflag.Int("metrics.database.options.buffer", 64, `database option buffer for audit database. Env "TAOS_KEEPER_METRICS_BUFFER"`)
+
+	viper.SetDefault("metrics.database.options.keep", 90)
+	_ = viper.BindEnv("metrics.database.options.keep", "TAOS_KEEPER_METRICS_KEEP")
+	pflag.Int("metrics.database.options.keep", 90, `database option buffer for audit database. Env "TAOS_KEEPER_METRICS_KEEP"`)
+
+	viper.SetDefault("metrics.database.options.cachemodel", "both")
+	_ = viper.BindEnv("metrics.database.options.cachemodel", "TAOS_KEEPER_METRICS_CACHEMODEL")
+	pflag.String("metrics.database.options.cachemodel", "both", `database option cachemodel for audit database. Env "TAOS_KEEPER_METRICS_CACHEMODEL"`)
+
 	viper.SetDefault("metrics.tables", []string{})
 	_ = viper.BindEnv("metrics.tables", "TAOS_KEEPER_METRICS_TABLES")
 	pflag.StringArray("metrics.tables", []string{}, `export some tables that are not super table, multiple values split with white space. Env "TAOS_KEEPER_METRICS_TABLES"`)
